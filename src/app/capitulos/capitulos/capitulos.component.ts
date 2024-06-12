3469cb072cbd0939356bf0343e8dcf6d5e9a0504
victorrivera <victorrivera004@gmail.com>
2024-06-11 22:58:27 -0500

    Paginacion lista

--- src/app/capitulos/capitulos/capitulos.component.ts
+++ src/app/capitulos/capitulos/capitulos.component.ts
@@ -5,17 +5,18 @@ import {MatIconModule} from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from '../../navbar/navbar/navbar.component';
+import {NgxPaginationModule} from 'ngx-pagination'; 
 
 @Component({
   selector: 'app-capitulos',
   standalone: true,
-  imports: [CommonModule, MatIconModule,MatListModule,HttpClientModule,NavbarComponent],
+  imports: [CommonModule, MatIconModule,MatListModule,HttpClientModule,NavbarComponent,NgxPaginationModule],
   providers: [RickAndMortyService],
   templateUrl: './capitulos.component.html',
   styleUrl: './capitulos.component.scss'
 })
 export class CapitulosComponent {
-
+  p: number = 1;
   capitulos: any;
   constructor(private rymService: RickAndMortyService) {}
   ngOnInit(): void {
--- src/app/locaciones/locaciones/locaciones.component.ts
+++ src/app/locaciones/locaciones/locaciones.component.ts
@@ -5,18 +5,20 @@ import {MatIconModule} from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from '../../navbar/navbar/navbar.component';
+import {NgxPaginationModule} from 'ngx-pagination'; 
 
 
 
 @Component({
   selector: 'app-locaciones',
   standalone: true,
-  imports: [CommonModule, MatIconModule,MatListModule,HttpClientModule,NavbarComponent],
+  imports: [CommonModule, MatIconModule,MatListModule,HttpClientModule,NavbarComponent,NgxPaginationModule],
   providers: [RickAndMortyService],
   templateUrl: './locaciones.component.html',
   styleUrl: './locaciones.component.scss'
 })
 export class LocacionesComponent {
+  p: number = 1;
   locaciones: any;
   constructor(private rymService: RickAndMortyService) {}
   ngOnInit(): void {
